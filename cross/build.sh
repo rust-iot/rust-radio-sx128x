a67b73e2dd97ad92ebb598343a1ee448ec60ec98
ryan <ryan@kurte.nz>
2020-03-10 15:02:13 +1300

    added bash script to simplify building

---
+++ cross/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DOCKER_CMD="docker run --rm -v=`pwd`:/work --workdir=/work --env PKG_CONFIG_ALLOW_CROSS=1"
+DOCKER_IMG=ryankurte/rust-embedded
+
+echo "Building for target: $1"
+
+# Args for cross build for armhf, windows tricks are in .cargo/config
+ARMHF_ARGS="--env SYSROOT=/usr/arm-linux-gnueabihf --env PKG_CONFIG_ALLOW_CROSS=1 --env PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig --env PKG_CONFIG_SYSROOT_DIR=/usr/arm-linux-gnueabihf --env PKG_CONFIG_SYSTEM_LIBRARY_PATH=/usr/lib/arm-linux-gnueabihf --env PKG_CONFIG_SYSTEM_INCLUDE_PATH=/usr/arm-linux-gnueabihf/include"
+
+
+if [ "$1" == "x86_64-unknown-linux-gnu" ]; then
+    $DOCKER_CMD $DOCKER_IMG /bin/bash -c "cargo build --target=$1 --release --features embedded-spi/hal-linux"
+
+elif [ "$1" == "armv7-unknown-linux-gnueabihf" ]; then
+    $DOCKER_CMD $ARMHF_ARGS $DOCKER_IMG /bin/bash -c "cargo build --target=$1 --release --features embedded-spi/hal-linux"
+
+elif [ "$1" == "x86_64-apple-darwin" ]; then
+    cargo build --target=$1 --release
+
+elif [ "$1" == "i686-pc-windows-gnu" ]; then
+    $DOCKER_CMD $DOCKER_IMG /bin/bash -c "cargo build --target=$1 --release"
+
+else
+
+    echo "Unsupported target: $1"
+    exit 1
+
+fi
